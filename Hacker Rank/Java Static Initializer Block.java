cd0b926e08941a0fb7a0ba17618bbf1ffa62d75a
Dishant Gupta <84134946+Dishant-jpg@users.noreply.github.com>
2023-07-05 19:09:06 +0530

    jjkgj

---
+++ Hacker Rank/Java Static Initializer Block.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Solution {
+    static int breadth;
+    static int height;
+    static boolean flag;
+
+    static {
+        Scanner scanner = new Scanner(System.in);
+        breadth = scanner.nextInt();
+        height = scanner.nextInt();
+        scanner.close();
+
+        if (breadth <= 0 || height <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
+        } else {
+            flag = true;
+        }
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = breadth * height;
+            System.out.println(area);
+        }
+    }
+}
