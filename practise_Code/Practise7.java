9a27a625710056580c247d4433a0277a89690517
Dishant Gupta <84134946+Dishant-jpg@users.noreply.github.com>
2023-07-13 12:32:45 +0530

    vsd

---
+++ practise_Code/Practise7.java
@@ -0,0 +1,18 @@
+package practise_Code;
+
+import java.util.Scanner;
+
+public class Practise7 {
+    public static void main(String[] args) {
+        int n =4;
+        int i = 1;
+        int sum = 0;
+
+        while(i<=n){
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("sum is :" + sum);
+    }
+}
