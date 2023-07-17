17423e730e99a3f2db65e61ad2895629c862e753
Dishant-jpggit config --global user.name Dishant-jpggit config --global user.email gupadishant90@gmail.comgit config --global user.name Dishant-jpg <84134946+Dishant-jpg@users.noreply.github.com>
2023-07-17 09:23:06 +0530

    b

---
+++ practise_Code/Untitled-1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class  {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a>0){
+            System.out.print( a + " is postive number");
+        } else{
+            System.out.print( a + " is negative number");
+        }
+    }
+}
